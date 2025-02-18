15c07eeddb314b6e9c59c2f286df0370665c1fe6
BMaster03 <bryan.rgg03@gmail.com>
2025-02-18 08:13:23 -0600

    red_2_actualizate

--- Neural_Networks/red_2.py
+++ Neural_Networks/red_2.py
@@ -26,13 +26,13 @@ print(y.shape)
 plt.scatter(x[:, 0], x[:, 1], c=y, s=40, cmap=plt.cm.viridis) 
 plt.show()
 
-def Sigmoid(X, derivate = False): # me return of derivative of fuction sigmoid
+def sigmoid(X, derivate = False): # me return of derivative of fuction sigmoid
     if derivate:
         return np.exp(-x)/(np.exp(-x)+1) ** 2
     else:
         return 1/(1 + np.exp(-x))
     
-def Relu(x, derivative = False):
+def relu(x, derivative = False):
     if derivative:
         x [x <= x] = 0
         x [x > 0 ] = 1
@@ -42,14 +42,29 @@ def Relu(x, derivative = False):
 
 # Loss of Fuction 
 
-def Mse(y,y_hat, derivative = False):
+def mse(y,y_hat, derivative = False):
     if derivative:
         return (y_hat - y)
     else:
         return np.mean((y_hat - y)) ** 2
 
+# Structure of red
+
+def initialize_parameters_deep(layers_dim: list): # Son parámetros cuando lo creamos, y argumentos cuando lo mandamos llamar
+    parameters = { }
+    L = len(layers_dim) # len = 4 [cantidad de elementos en la lista]: te dice cuantos elementos tiene el argumento, dir: me dice que métodos y atributos tiene una función en python 
+    for l in range(0, L-1): # el range y su argumento es un iterador, o generador, que en este caso de num. entre el 0 y L-1
+        parameters['W' + str(l + 1)] = (np.random.rand( # que genere una matriz de 2 * 4 de valores aleatorios
+            layers_dim[l], layers_dim[l + 1]) * 2) - 1 # generación de los pesos de la primera capa
+        
+        parameters['b' + str(l + 1)] = (np.random.rand( # que genere una matriz de 2 * 4 de valores aleatorios
+            1, layers_dim[l + 1]) * 2) - 1 # que los valores estén desde 0 a 1
+        
+    return parameters
+layers_dims = [2,4,8,1]
+
+params = initialize_parameters_deep(layers_dims)
+print(params)
 
 
 
-
-    
